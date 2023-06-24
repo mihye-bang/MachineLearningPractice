895f78056c679d6804c6e0d0c4ef58203ed2b894
Mihye <beasjnml@nate.com>
2023-06-24 01:19:34 -0400

    read csv data, fit in linear regression, and showcased dotted data with predicted linear regression

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
 from sklearn.linear_model import LinearRegression
 import pandas as pd
 import numpy as np
-import matplotlib.pylplot as plt
+import matplotlib.pyplot as plt
 
+df = pd.read_csv("heights.csv")
+# returns the first five rows
+df.head()
+
+x = df["height"]
+y = df["weight"]
 line_fitter = LinearRegression()
+line_fitter.fit(x.values.reshape(-1, 1), y)
+
+plt.plot(x, y, 'o')
+plt.plot(x, line_fitter.predict(x.values.reshape(-1, 1)))
+plt.show()
+
+
 # fit : passing two variables for linear regression model
-line_fitter.fit(x, y)
+# reason for reshaping: x should be of 2 dimentional array. [[x1], [x2], [x3], ... , [xn]]
+
+
+print(line_fitter.predict([[70]]))
+print(line_fitter.coef_)
+print(line_fitter.intercept_)
